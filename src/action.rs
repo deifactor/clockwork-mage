1fe33beed0bec882e39a58a73ef94cee35b43064
Ash <relativistic.policeman@gmail.com>
2018-10-01 23:00:11 -0700

    Add some dummy actions.

--- src/action.rs
+++ src/action.rs
@@ -1,7 +1,29 @@
 /// Represents an individual action that the player uses (spell, weaponskill,
 /// oGCD, etc.).
+use crate::time::*;
+
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum Action {
     Hit,
-    Recharge
+    Recharge,
+}
+
+impl Action {
+    pub fn cast_time(&self) -> Duration {
+        match self {
+            Action::Hit => Duration(150),
+            Action::Recharge => Duration(250),
+        }
+    }
+
+    pub fn recast_time(&self) -> Duration {
+        Duration(250)
+    }
+
+    pub fn mp_cost(&self) -> i32 {
+        match self {
+            Action::Hit => 1000,
+            Action::Recharge => 2000,
+        }
+    }
 }
